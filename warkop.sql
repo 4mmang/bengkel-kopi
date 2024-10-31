523e4af902090998cd207bbf8daa4b70ee781b21
4mmang <ammangdeveloper@gmail.com>
2024-10-31 12:46:24 +0800

    [fix] menyelesaikan database

--- warkop.sql
+++ warkop.sql
@@ -23,16 +23,16 @@ CREATE TABLE IF NOT EXISTS `menus` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Dumping data for table warkop.menus: ~19 rows (approximately)
+-- Dumping data for table warkop.menus: ~17 rows (approximately)
 INSERT INTO `menus` (`id`, `nama`, `harga`, `status`) VALUES
-	(1, 'Air Mineral', 5000, 'off'),
+	(1, 'Air Mineral', 5000, 'on'),
 	(2, 'Kopi Hitam', 5000, 'off'),
 	(3, 'Kopi Klasik', 5000, 'off'),
 	(4, 'Kopi Tubruk Susu', 5000, 'off'),
 	(5, 'Vietnam Drip', 10000, 'off'),
 	(6, 'Ice Kopi Susu BK', 13000, 'off'),
 	(7, 'Ice Espresso X Matcha', 15000, 'off'),
-	(8, 'Ice Espresso X Aren', 15000, 'off'),
+	(8, 'Ice Espresso X Aren', 15000, 'on'),
 	(9, 'Ice Espresso X Banana', 15000, 'off'),
 	(10, 'Ice Espresso X Avocado', 15000, 'off'),
 	(11, 'Ice Espresso X Vanilla', 15000, 'off'),
@@ -52,15 +52,13 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `id_menu` int NOT NULL,
   `jumlah` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Dumping data for table warkop.orders: ~5 rows (approximately)
+-- Dumping data for table warkop.orders: ~0 rows (approximately)
 INSERT INTO `orders` (`id`, `id_pesanan`, `id_menu`, `jumlah`) VALUES
-	(1, 1, 7, 4),
-	(2, 1, 1, 1),
-	(3, 2, 12, 2),
-	(4, 3, 10, 1),
-	(5, 4, 1, 1);
+	(1, 1, 1, 2),
+	(2, 2, 8, 2),
+	(3, 2, 1, 1);
 
 -- Dumping structure for table warkop.pesanan
 CREATE TABLE IF NOT EXISTS `pesanan` (
@@ -71,14 +69,24 @@ CREATE TABLE IF NOT EXISTS `pesanan` (
   `status` enum('proses','selesai') COLLATE armscii8_bin NOT NULL DEFAULT 'proses',
   `waktu_pemesanan` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Dumping data for table warkop.pesanan: ~4 rows (approximately)
+-- Dumping data for table warkop.pesanan: ~0 rows (approximately)
 INSERT INTO `pesanan` (`id`, `nama`, `meja`, `take_away`, `status`, `waktu_pemesanan`) VALUES
-	(1, 'Arman', 'luar', 'ya', 'selesai', '2024-10-30 18:36:20'),
-	(2, 'Allcode', 'dalam', 'ya', 'selesai', '2024-10-30 18:44:06'),
-	(3, 'Hendri', 'dalam', 'ya', 'selesai', '2024-10-30 18:45:49'),
-	(4, 'ARMAN UMAR', 'dalam', 'tidak', 'selesai', '2024-10-30 18:50:52');
+	(1, 'Arman', 'dalam', 'tidak', 'selesai', '2024-10-31 06:52:39'),
+	(2, 'Allcode', 'luar', 'ya', 'selesai', '2024-10-31 07:48:27');
+
+-- Dumping structure for table warkop.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE armscii8_bin NOT NULL,
+  `password` varchar(255) CHARACTER SET armscii8 COLLATE armscii8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+
+-- Dumping data for table warkop.users: ~0 rows (approximately)
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+	(1, 'admin', '$2y$10$Bg7MSUbxf5G57EKvKE640OUmDTpsuwMMvdG84BBhfvBw.qPn1IFXC');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
